18b92fc1f7c94c14efb2f759ebc8266503c06ec0
Ryan <tripper@TripperdeMacBook-Pro.local>
2016-08-04 02:52:25 +0800

    add tag for loading base media

--- forms.py
+++ forms.py
@@ -3,11 +3,20 @@ from django import forms
 class BaseMadiaWidget(forms.TextInput):
     class Media:
         css = {
-            'all': ('admin/css/base.css',
+            'all': (
                     'admin/css/bootstrap.css',
-                    'admin/css/fonts.css',
+                    'admin/vendor/fontawesome/css/font-awesome.min.css',
+                    'admin/vendor/animo/animate+animo.css',
+                    'admin/vendor/csspinner/csspinner.min.css',
+                    'admin/css/app.css',
                     )
         }
-        js = ('admin/js/collapse.js',
-              'admin/js/actions.js'
+        js = (
+              'admin/vendor/modernizr/modernizr.js',
+              'admin/vendor/fastclick/fastclick.js',
+              'admin/vendor/jquery/jquery.min.js',
+              'admin/vendor/bootstrap/js/bootstrap.min.js',
+              'admin/vendor/animo/animo.min.js',
+              'admin/js/app.js'
               )
+
