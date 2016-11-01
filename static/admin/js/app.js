e09fe405cd6a743526b6d32dfb8237e9c66cd53d
Andy <Andy@AndydeMacBook-Pro.local>
2016-11-01 23:28:01 +0800

    fix

--- static/admin/js/app.js
+++ static/admin/js/app.js
@@ -1211,7 +1211,7 @@
     Markdownarea.defaults = {
         "mode"         : "split",
         "height"       : 500,
-        "maxsplitsize" : 800,
+        "maxsplitsize" : 600,
         "codemirror"   : { mode: 'gfm', tabMode: 'indent', tabindex: "2", lineWrapping: true, dragDrop: false, autoCloseTags: true, matchTags: true },
         "toolbar"      : [ "bold", "italic", "strike", "link", "picture", "blockquote", "listUl", "listOl" ],
         "lblPreview"   : "Preview",
