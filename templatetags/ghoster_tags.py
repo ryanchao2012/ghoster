5df7403f395000c08dbd947e6e5b279af333d2c3
Ryan <tripper@TripperdeMacBook-Pro.local>
2016-11-08 17:24:46 +0800

    fix ghoster_change_form2.html layout

--- templatetags/ghoster_tags.py
+++ templatetags/ghoster_tags.py
@@ -6,3 +6,11 @@ register = template.Library()
 @register.simple_tag
 def get_base_media():
     return forms.BaseMadiaWidget().media
+
+
+@register.simple_tag
+def render_post_form(post):
+    content = forms.GhosterContentForm(instance = post)
+    meta = forms.GhosterMetaForm(instance = post)
+    return {'content': content['content'], 'meta': meta}
+
