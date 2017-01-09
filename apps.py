39bf62a820a968cdc6a00479ad9e02a05c6d4aff
Ryan <tripper@TripperdeMacBook-Pro.local>
2017-01-09 20:26:28 +0800

    adapt FileField in widgets.py

--- apps.py
+++ apps.py
@@ -4,6 +4,7 @@ from django import db
 from django.forms.widgets import Select, CheckboxInput
 from django.contrib.admin import options
 from .widgets import GhosterTextInputWidget, GhosterWidget
+    # GhosterURLFieldWidget
 
 
 
@@ -13,8 +14,9 @@ class GhosterConfig(AppConfig):
     verbose_name = "GHOSTER CMS"
     settings.TEMPLATES[0]['OPTIONS']['context_processors'].append('ghoster.context_processors.application')
     Select.render = GhosterWidget.select_render
-    # CheckboxInput.render = GhosterWidget.slider_checkbox_render
+    CheckboxInput.render = GhosterWidget.slider_checkbox_render
     options.FORMFIELD_FOR_DBFIELD_DEFAULTS[db.models.CharField].update({'widget': GhosterTextInputWidget})
+
     # options.FORMFIELD_FOR_DBFIELD_DEFAULTS[db.models.CharField]['widget'] = GhosterTextInputWidget
 
 
--- widgets.py
+++ widgets.py
@@ -1,9 +1,14 @@
 from django import forms
+from django.forms import ClearableFileInput
 from django.utils.safestring import mark_safe
 from django.utils.html import format_html
 from django.forms.utils import flatatt
 from django.utils.encoding import force_text
-from django.forms.widgets import ChoiceInput
+from django.forms.widgets import CheckboxInput
+from django.utils.html import conditional_escape
+from django.utils.html import smart_urlquote
+from django.utils.translation import ugettext as _
+
 class GhosterTextInputWidget(forms.TextInput):
     def __init__(self, attrs=None):
         final_attrs = {'class': 'form-control'}
@@ -11,6 +16,45 @@ class GhosterTextInputWidget(forms.TextInput):
             final_attrs.update(attrs)
         super(GhosterTextInputWidget, self).__init__(attrs=final_attrs)
 
+
+
+class GhosterURLFieldWidget(ClearableFileInput):
+    template_default = (
+        '<div class="btn btn-primary btn-block">'
+        '<i class="fa fa-cloud-upload fa-lg">%(input)s</i>'
+        '</div>'
+    )
+    template_with_initial = (
+        '<img src="%(initial_url)s" width="200" /><br />'
+        + template_default +
+        '%(clear_template)s'
+        # '<p class="file-upload">%s</p>' % forms.ClearableFileInput.template_with_initial
+    )
+    template_with_clear = (
+        '%s' % forms.ClearableFileInput.template_with_clear
+    )
+
+    def render(self, name, value, attrs=None):
+        substitutions = {
+            'initial_text': self.initial_text,
+            'input_text': self.input_text,
+            'clear_template': '',
+            'clear_checkbox_label': self.clear_checkbox_label,
+        }
+        template = GhosterURLFieldWidget.template_default
+        substitutions['input'] = super(ClearableFileInput, self).render(name, value, attrs)
+
+        if self.is_initial(value):
+            template = self.template_with_initial
+            substitutions.update(self.get_template_substitution_values(value))
+            if not self.is_required:
+                checkbox_name = self.clear_checkbox_name(name)
+                checkbox_id = self.clear_checkbox_id(checkbox_name)
+                substitutions['clear_checkbox_name'] = conditional_escape(checkbox_name)
+                substitutions['clear_checkbox_id'] = conditional_escape(checkbox_id)
+                substitutions['clear'] = CheckboxInput().render(checkbox_name, False, attrs={'id': checkbox_id})
+                substitutions['clear_template'] = self.template_with_clear % substitutions
+        return mark_safe(template % substitutions)
 
 class GhosterWidget(object):
 
