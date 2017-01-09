fc52a5b006551c1a42b00ce633af525d3d0dbaf7
Ryan <tripper@TripperdeMacBook-Pro.local>
2017-01-09 22:03:35 +0800

    adapt Tag field

--- apps.py
+++ apps.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django import db
 from django.forms.widgets import Select, CheckboxInput
 from django.contrib.admin import options
-from .widgets import GhosterTextInputWidget, GhosterWidget
+from .widgets import GhTextInputWidget, GhosterWidget
     # GhosterURLFieldWidget
 
 
@@ -15,7 +15,7 @@ class GhosterConfig(AppConfig):
     settings.TEMPLATES[0]['OPTIONS']['context_processors'].append('ghoster.context_processors.application')
     Select.render = GhosterWidget.select_render
     CheckboxInput.render = GhosterWidget.slider_checkbox_render
-    options.FORMFIELD_FOR_DBFIELD_DEFAULTS[db.models.CharField].update({'widget': GhosterTextInputWidget})
+    options.FORMFIELD_FOR_DBFIELD_DEFAULTS[db.models.CharField].update({'widget': GhTextInputWidget})
 
     # options.FORMFIELD_FOR_DBFIELD_DEFAULTS[db.models.CharField]['widget'] = GhosterTextInputWidget
 
