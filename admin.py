5c020b82730629f45b6043c037ea13fd6ae63ec5
Ryan <tripper@TripperdeMacBook-Pro.local>
2017-02-08 02:30:30 +0800

    populate ghosterform into ghoster_change_form_html

--- admin.py
+++ admin.py
@@ -5,7 +5,5 @@ from django.contrib import admin
 class GhosterAdmin(admin.ModelAdmin):
     markdown_field = None
     title_field = None
-    meta_fields = None
     add_form_template = 'admin/ghoster_change_form.html'
     change_form_template = 'admin/ghoster_change_form.html'
-
--- templatetags/ghoster_tags.py
+++ templatetags/ghoster_tags.py
@@ -1,9 +1,10 @@
 from django import template
 from ghoster import forms
 from django.contrib import admin
+from django.contrib.admin import helpers
 import pprint
-# from django.contrib.admin.utils import flatten_fieldsets
-
+import copy
+import re
 register = template.Library()
 
 
@@ -12,8 +13,6 @@ def flatten(fields):
     original list."""
     flat = []
     for field in fields:
-        # if field == 'text_field':
-        #     continue
         if isinstance(field, (list, tuple)):
             flat.extend(field)
         else:
@@ -29,6 +28,29 @@ def flatten_fieldsets(fieldsets):
         )
     return flat
 
+
+def get_meta_fieldsets(flat_fieldsets, model_admin):
+    markdown_field_name = model_admin.markdown_field
+    title_field_name = model_admin.title_field
+    meta_fieldsets = []
+    has_markdown = False
+    has_title = False
+    for name, opts in flat_fieldsets:
+        if not has_markdown:
+            if markdown_field_name in opts['fields']:
+                opts['fields'].remove(markdown_field_name)
+                has_markdown = True
+
+        if not has_title:
+            if title_field_name in opts['fields']:
+                opts['fields'].remove(title_field_name)
+                has_title = True
+
+        meta_fieldsets.append(
+            (name, {'fields': opts['fields']})
+        )
+    return meta_fieldsets
+
 @register.simple_tag
 def get_base_media():
     pass
@@ -39,30 +61,34 @@ def get_base_media():
 def get_gh_app_list(context):
     return admin.site.get_app_list(context['request'])
 
-@register.simple_tag
-def render_post_form(adminform):
-    try:
-        model_admin = adminform.model_admin
-        fieldsets = flatten_fieldsets(adminform.fieldsets)
-        adminform.fieldsets = fieldsets
-    except:
-        pass
-    print('-----adminform-----')
-    pprint.pprint(adminform.__dict__, width=1)
+@register.simple_tag()
+def get_ghoster_form(adminform):
+    form = adminform.form
+    model_admin = adminform.model_admin
+    readonly_fields = adminform.readonly_fields
+    flat_fieldsets = flatten_fieldsets(adminform.fieldsets)
+    prepopulated_fields = adminform.prepopulated_fields
+    if prepopulated_fields:
+        prepopulated_fields = prepopulated_fields[0]
+    else: 
+        prepopulated_fields = {}
 
-    for fieldset in adminform:
-        print('---fieldset---')
-        pprint.pprint(fieldset.__dict__, width=1)
-        for line in fieldset:
-            print('--line--')
-            pprint.pprint(line.__dict__, width=1)
-            for field in line:
-                print('@')
-                pprint.pprint(field.__dict__, width=1)
+    meta_fieldsets = get_meta_fieldsets(flat_fieldsets, model_admin)
 
-    # pprint.pprint(form.form.fields['char_field'].widget, width=1)
-    return adminform
-#     content = forms.GhosterContentForm(instance = post)
-#     meta = forms.GhosterMetaForm(instance = post)
-#     return {'content': content['content'], 'meta': meta}
+    markdownfield = form[model_admin.markdown_field]
+    markdownfield.id = re.search(r'id=\"(\S+)\"', str(markdownfield)).group(1)
+    
+    titlefield = form[model_admin.title_field]
+    titlefield.field.widget.attrs['class'] += ' form-control'
+    
+    metaformsets = helpers.AdminForm(
+        form, 
+        meta_fieldsets, 
+        prepopulated_fields, 
+        readonly_fields=readonly_fields, 
+        model_admin=model_admin
+    )
+
+    return {'markdown': markdownfield, 'title': titlefield, 'meta': metaformsets}
+
 
